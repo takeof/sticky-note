4b5c63873029c8a9c48e4aef4892f969840077b5
takeof <takeof.nakahara@gmail.com>
2022-09-25 13:42:25 +0900

    server-side error display

--- src/routes/notes/+server.js
+++ src/routes/notes/+server.js
@@ -3,7 +3,7 @@ import { json } from '@sveltejs/kit';
 import { Twilio } from 'twilio';
 
 const accountSid = 'AC6f3aa9045df387be43f57805de676287';
-const authToken = 'a2611006ab90cc76a32eb55d7ce3d349'; 
+const authToken = '2611006ab90cc76a32eb55d7ce3d349'; 
 const syncServiceSid = 'IS4eaa7cb1252aa072dca3a8eb14020b81';
 const syncListSid = 'ESf0a8237d4035be5be1f4f84ad9fbe04e';
 
@@ -35,6 +35,10 @@ export async function GET() {
             })
             console.log(`syncList item: ${item.data.memo} ${item.data.text} ${Date.now()}`);
         });
+    })
+    .catch((error) => {
+        console.log(error);
+        list.push({index: 0, text: error.message, top: 0});
     });
     // list.sort((a, b) => {
     //     if (a.limit == b.limit) {
